223c282f3db955bd15ad7396ca5e3b77bd326113
Jonas Lalin <1ac6df24@gmail.com>
2024-06-07 14:32:39 +0200

    model2

--- src/onnxplayground/model2.py
+++ src/onnxplayground/model2.py
@@ -3,20 +3,22 @@ import numpy.typing as npt
 from onnx import ModelProto, TensorProto
 from onnx.checker import check_model
 from onnx.helper import make_graph, make_model, make_node, make_tensor_value_info
+from onnx.numpy_helper import from_array
 from onnx.reference import ReferenceEvaluator
 
 
 def make_my_model() -> ModelProto:
-    X = make_tensor_value_info("X", TensorProto.FLOAT, [None, None])
-    A = make_tensor_value_info("A", TensorProto.FLOAT, [None, None])
-    B = make_tensor_value_info("B", TensorProto.FLOAT, [None, None])
+    X = make_tensor_value_info("X", TensorProto.FLOAT, [None, 2])  # m x 2
+
+    A = from_array(np.array([[1, 2, 3], [4, 5, 6]], np.float32), "A")  # 2 x 3
+    B = from_array(np.array([[1, 2, 3]], dtype=np.float32), "B")  # 1 x 3
 
     node1 = make_node("MatMul", ["X", "A"], ["XA"])
     node2 = make_node("Add", ["XA", "B"], ["Y"])
 
-    Y = make_tensor_value_info("Y", TensorProto.FLOAT, [None, None])
+    Y = make_tensor_value_info("Y", TensorProto.FLOAT, [None, 3])  # m x 3
 
-    g = make_graph([node1, node2], "g", [X, A, B], [Y])
+    g = make_graph([node1, node2], "g", [X], [Y], [A, B])
 
     model = make_model(g)
     check_model(model)
@@ -27,11 +29,9 @@ if __name__ == "__main__":
     my_model = make_my_model()
     print(my_model)
 
-    X = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float32)  # 2 x 3
-    A = np.array([[1, 2], [3, 4], [5, 6]], dtype=np.float32)  # 3 x 2
-    B = np.array([[1]], dtype=np.float32)  # 1 x 1
+    X = np.array([[1, 2], [3, 4]], dtype=np.float32)
 
     sess = ReferenceEvaluator(my_model, verbose=4)
-    output: npt.NDArray[np.float32] = sess.run(None, {"X": X, "A": A, "B": B})[0]
+    output: npt.NDArray[np.float32] = sess.run(None, {"X": X})[0]
     print(output)
     print(output.shape)
